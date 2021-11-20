6915c59e2716fdaace5cedc67d89751ad4226d74
zhouyi <shzy2012@gmail.com>
2021-11-20 13:47:52 +0800

    add format

---
+++ examples/format.rs
@@ -0,0 +1,38 @@
+/*
+https://doc.rust-lang.org/rust-by-example/hello/print.html
+Printing is handled by a series of macros defined in std::fmt some of which include:
+
+format!: write formatted text to String
+print!: same as format! but the text is printed to the console (io::stdout).
+println!: same as print! but a newline is appended.
+eprint!: same as format! but the text is printed to the standard error (io::stderr).
+eprintln!: same as eprint!but a newline is appended.
+*/
+fn main() {
+    // In general, the `{}` will be automatically replaced with any
+    // arguments. These will be stringified.
+    println!("{} days", 31);
+
+    // Without a suffix, 31 becomes an i32. You can change what type 31 is
+    // by providing a suffix. The number 31i64 for example has the type i64.
+
+    // There are various optional patterns this works with. Positional
+    // arguments can be used.
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+     // As can named arguments.
+     println!("{subject} {verb} {object}",
+     object="the lazy dog",
+     subject="the quick brown fox",
+     verb="jumps over");
+
+    // Special formatting can be specified after a `:`.
+    println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
+
+    // You can right-align text with a specified width. This will output
+    // "     1". 5 white spaces and a "1".
+    println!("{number:>width$}", number=1, width=6);
+
+    // You can pad numbers with extra zeroes. This will output "000001".
+    println!("{number:0>width$}", number=1, width=6);
+}
---
+++ examples/hello.rs
@@ -0,0 +1,12 @@
+// This is the main function
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
+
+/* Hello world
+$ rustc hello.rs
+$ ./hello
+*/
